d12c945c365bef7fc9bbfe7bbaf24ccea111bac7
vigneshsuresh <vigneshsuresh1775@gmail.com>
2020-07-02 21:32:12 +0530

    added initial changes

--- summarization/stamp_page_picking/interesting_sequence_picker.py
+++ summarization/stamp_page_picking/interesting_sequence_picker.py
@@ -1,6 +1,8 @@
 ''' A module to find the most interesting sequence
 of stamp pages that can be displayed to the user
 '''
+from sklearn.metrics.pairwise import cosine_similarity
+
 from summarization.stamp_page_picking.max_cover_preprocessor import \
     BudgetedMaxCoverPreprocessor
 
@@ -23,6 +25,10 @@ class InterestingSequencePicker:
         else stop
     '''
 
+    # threshold as used in the determining the cover
+    # of the stamp page
+    THRESHOLD = 0.5  # change as required
+
     def __init__(
             self,
             stamp_pages,
@@ -43,11 +49,7 @@ class InterestingSequencePicker:
         # the first stamp page is the title card
         # for the series of stamp pages
         # we will use that as the seed stamp pages
-        self.last_picked_stamp_page_index = self.stamp_pages[0]
-        self._pick_cover_at_index(0)
-        self.stamp_page_indices.pop(0)
-
-        self.threshold = 0.5  # change as required
+        self._set_seed_stamp_page()
 
         # the final sequence that will be returned
         self.stamp_page_sequence = list()
@@ -58,7 +60,9 @@ class InterestingSequencePicker:
             1,  # content change score weight
             1,  # sentiment change score
             1,  # textual entailment score
-            1,  # unpicked weights score
+            1,  # unpicked weights/cost score - as used in max cover
+                # it tells how many sentences in the web-page are
+                # unpicked yet and its ratio to stamp page cost
             1   # sweeping index score
         ]
 
@@ -73,7 +77,7 @@ class InterestingSequencePicker:
         self.step_size = 1
 
     def get_interesting_sequence(self):
-        for i in range(self.max_pages_allowed):
+        for self.iteration_count in range(self.max_pages_allowed):
             # get the index of the next best stamp page
             stamp_page_index = self._get_next_best_stamp_page_index()
 
@@ -89,7 +93,7 @@ class InterestingSequencePicker:
 
             # update the cover for unpicked weights
             # calculation during next iteration
-            self._pick_cover_at_index(stamp_page_index)
+            self._pick_stamp_page_cover_at_index(stamp_page_index)
 
             # update the sweeping index
             self._update_sweeping_index()
@@ -99,20 +103,114 @@ class InterestingSequencePicker:
 
         return self.stamp_page_sequence
 
+    def _get_next_best_stamp_page_index(self):
+        ''' returns the index of the
+        next best stamp page to pick given knowledge
+        of the last picked stamp page
+        '''
+
+        if len(self.stamp_page_indices) == 0:
+            return -1
+
+        # sort based on interestingness metric
+        self.stamp_page_indices.sort(
+            key=self._interestingess_metric, reverse=True)
+
+        # stamp page index at index 0 will have highest score
+        next_best_index = self.stamp_page_indices[0]
+
+        # pop the index from indices list so it's not
+        # picked again
+        self.stamp_page_indices.pop(0)
+
+        return next_best_index
+
+    def _interestingess_metric(self, stamp_page_index):
+        '''
+        calculates the interesting-ness metric based
+        on the last picked stamp page
+        '''
+        from_stamp_page_index = self.last_picked_stamp_page_index
+        to_stamp_page_index = stamp_page_index
+
+        content_change_score \
+            = self._get_content_change_score(
+                from_stamp_page_index,
+                to_stamp_page_index
+            )
+
+        sentiment_change_score \
+            = 0  # will be amended once sentiment detection PR is added
+
+        content_similarity_score = self._get_content_similarity_score(
+            from_stamp_page_index, to_stamp_page_index)
+
+        unpicked_weight_cost_ratio_score \
+            = self._get_unpicked_weights_to_cost_ratio_score(stamp_page_index)
+
+        sweeping_index_score = self._get_distance_from_sweeping_index(
+            stamp_page_index)
+
+        return self._compute_weighted_score(
+            [
+                content_change_score,
+                sentiment_change_score,
+                content_similarity_score,
+                unpicked_weight_cost_ratio_score,
+                sweeping_index_score
+            ]
+        )
+
+    def _set_seed_stamp_page(self):
+        ''' This is the stamp page from which we'll
+        construct our sequence
+        '''
+        self.last_picked_stamp_page_index = self.stamp_pages[0]
+        self._set_stamp_page_covers()
+        self._pick_stamp_page_cover_at_index(0)
+        self.stamp_page_indices.pop(0)
+
     def _set_stamp_page_covers(self):
         ''' find covers for stamp pages'''
         preprocessor = BudgetedMaxCoverPreprocessor(
             self.stamp_pages,
             self.summary_sentence_embeddings,
-            self.threshold)
+            self.THRESHOLD)
         self.stamp_page_covers \
             = preprocessor.get_cover_objects_for_stamp_pages()
 
-    def _pick_cover_at_index(self, index):
+    def _pick_stamp_page_cover_at_index(self, index):
         # update the picked_cover
         for i in range(self.cover_size):
             self.picked_cover[i] \
                 = self.picked_cover[i] | self.stamp_page_covers[index].cover[i]
+
+    def _get_content_change_score(self, from_index, to_index):
+        # higher change in content type is better
+        return abs(
+            self._get_stamp_page_type(from_index),
+            self._get_stamp_page_type(to_index)
+        )
+
+    def _get_unpicked_weights_to_cost_ratio_score(self, index):
+        total_weight = 0
+        for i in range(self.cover_size):
+            if self.picked_cover[i] == 0 \
+                    and self.stamp_page_covers[index].cover[i] == 1:
+                total_weight += 1
+        return total_weight / self.stamp_page_covers[index].cost
+
+    def _compute_weighted_score(self, score_list):
+        return sum(
+            [a * b for a, b in zip(score_list, self.individual_score_weights)]
+        )
+
+    def _get_content_similarity_score(self, from_index, to_index):
+        ''' Gets the similarity with the previously picked stamp page'''
+        return cosine_similarity(
+            self.stamp_pages[from_index].stamp_descriptor_embedding,
+            self.stamp_pages[to_index].stamp_descriptor_embedding
+        )
 
     def _get_stamp_page_type(self, stamp_page_index):
         # assign a number to each stamp page type
@@ -125,26 +223,6 @@ class InterestingSequencePicker:
             return 3
         elif stamp_page.sentence_index != -1:
             return 4
-
-    def _get_content_change_score(self, from_index, to_index):
-        # higher change in content type is better
-        return abs(
-            self._get_stamp_page_type(from_index),
-            self._get_stamp_page_type(to_index)
-        )
-
-    def _get_unpicked_weights_to_cost_ratio(self, index):
-        total_weight = 0
-        for i in range(self.cover_size):
-            if self.picked_cover[i] == 0 \
-                    and self.stamp_page_covers[index].cover[i] == 1:
-                total_weight += 1
-        return total_weight / self.stamp_page_covers[index].cost
-
-    def _compute_weighted_score(self, score_list):
-        return sum(
-            [a * b for a, b in zip(score_list, self.individual_score_weights)]
-        )
 
     def _get_approximate_index_for_stamp_page(self, stamp_page_index):
         '''Fetches the approximate index so it can be used
@@ -169,65 +247,5 @@ class InterestingSequencePicker:
             stamp_page_index)
         return abs(self.sweeping_index - approximate_index)
 
-    def _interestingess_metric(self, stamp_page_index):
-        '''
-        calculates the interesting-ness metric based
-        on the last picked stamp page
-        '''
-        from_stamp_page_index = self.last_picked_stamp_page_index
-        to_stamp_page_index = stamp_page_index
-
-        content_change_score \
-            = self._get_content_change_score(
-                from_stamp_page_index,
-                to_stamp_page_index
-            )
-
-        sentiment_change_score \
-            = 0  # will be amended once sentiment detection PR is added
-
-        textual_entailment_score \
-            = 0  # will be amended once textual entailment PR is added
-
-        unpicked_weight_cost_ratio_score \
-            = self._get_unpicked_weights_to_cost_ratio(stamp_page_index)
-
-        sweeping_index_score = self._get_distance_from_sweeping_index(
-            stamp_page_index)
-
-        return self._compute_weighted_score(
-            [
-                content_change_score,
-                sentiment_change_score,
-                textual_entailment_score,
-                unpicked_weight_cost_ratio_score,
-                sweeping_index_score
-            ]
-        )
-
     def _update_sweeping_index(self):
-        # TODO: amend this to have variable
-        # change based on iteration
-        self.sweeping_index -= self.step_size
-
-    def _get_next_best_stamp_page_index(self):
-        ''' returns the index of the
-        next best stamp page to pick given knowledge
-        of the last picked stamp page
-        '''
-
-        if len(self.stamp_page_indices) == 0:
-            return -1
-
-        # sort based on interestingness metric
-        self.stamp_page_indices.sort(
-            key=self._interestingess_metric, reverse=True)
-
-        # stamp page index at index 0 will have highest score
-        next_best_index = self.stamp_page_indices[0]
-
-        # pop the index from indices list so it's not
-        # picked again
-        self.stamp_page_indices.pop(0)
-
-        return next_best_index
+        self.sweeping_index -= self.step_size * self.iteration_count
